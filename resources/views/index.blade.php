02d50957926f8cca2726311ef35c5123ec5be8a8
VInicius-Mance <vinimanceprof@gmail.com>
2020-11-18 22:10:25 -0300

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Home</title>
+  </head>
+  <body>
+     <header>
+        <nav> <a href="/" id="mainHeaderText" class="headerText">Main header text</a>
+          <a href="/" class="headerText">&laquo; Secondary header text</a>
+          <a target="_blank" href="https://www.linkedin.com/in/rogério-rodrigues-86608812"><img id="logo" class="headerText" src="img/linkedInLogo.png" alt="linkedin"></a>
+        </nav>
+     </header>
+    <main id="indexMainBanner">
+      <img id="indexMainBannerBG" class="banner" src="img/indexMainBanner.jpeg" alt="banner">
+      <div class="upperBanner">
+        <img id="upperBannerLogo" src="img/linkedInLogo.png" alt="">
+          <a href="#bannertest"><h1 id="upperBannerText">Main banner text here Pack my box with five dozen liquor jugs <br>&darr;</h1></a>
+      </div>
+    </main>
+    <main id="bannertest">
+    </main>
+  </body>
+</html>
